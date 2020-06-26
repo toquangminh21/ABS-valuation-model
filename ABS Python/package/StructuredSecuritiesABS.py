57abdf56b7d2e25aa5e7f19e90f6c902102730fe
Minh To <65303559+toquangminh21@users.noreply.github.com>
2020-06-26 12:05:04 -0400

    added Monte Carlo capabilities

--- ABS Python/package/StructuredSecuritiesABS.py
+++ ABS Python/package/StructuredSecuritiesABS.py
@@ -4,7 +4,7 @@ Monday, 6/15/2020: Get back
 StructuredSecurities class
 '''
 
-import logging
+import logging, math
 from package.Tranche import StandardTranche
 from package.LoanPool import LoanPool
 
@@ -44,21 +44,28 @@ class StructuredSecurities(object):
             yield tranche
 
 
-    # Factory method to add tranches to the StructuredSecurity object
-    def addTranche(self, trancheClass, percentNotional, rate, subordination):
-        # tranceClass: StandardTranche, IOTrance, or POTrance
-        # percentNotional: percentage of the deal's notional the tranche has
-        # rate: rate of the tranche
-        # subordination: is the tranche senior or subordinated? 'A' is senior, 'B' is subordinated
 
+    def addTranche(self, trancheClass, percentNotional, rate, subordination):
+        '''
+        # Factory method to add tranches to the StructuredSecurity object
+        :param trancheClass: StandardTranche, IOTrance, or POTrance
+        :param percentNotional: percentage of the deal's notional the tranche has
+        :param rate: rate of the tranche
+        :param subordination: is the tranche senior or subordinated? 'A' is senior, 'B' is subordinated
+        :return: None
+        '''
         # Instantiate the tranche
         tranche = trancheClass(self.totalNotional * percentNotional, rate, subordination)
         tranche.percentNotional = percentNotional
         # Append the tranche to the StructuredSecurities internal trancheList
         self.trancheList.append(tranche)
 
-    # Method to increment the time period of the underlying tranches and the SS object itself
     def increaseTimePeriod(self):
+        '''
+        Method to increment the time period of the underlying tranches and the SS object itself
+        :return:
+        '''
+
         for tranche in self:
             tranche.increaseTimePeriod()
         # Also increment period to keep up with the tranches and to calculate the principalDue accurately
@@ -92,9 +99,7 @@ class StructuredSecurities(object):
         principalDue = self.loanPool.totalPrincipalDue(self.period)
 
 
-        ##### Principal payments, assuming there's money left after interest payments have been made
-        # The residual Principal payments due to the tranches are based on the total principal received from the LOANPOOL...
-        # ... NOT the total available cash
+        # In this loop, principalDue refers to the TRANCHE's principal due
         for tranche in self:
 
             # Sequential payout: Senior tranche gets all the principal due if cashAmount allows.
@@ -112,9 +117,11 @@ class StructuredSecurities(object):
             principalPayment = min(principalDue, cashAmount, tranche.properties[tranche.period - 1]['Notional balance'])
 
             ##### Making payments, recording them, and reducing cash and principal due for each tranche
-
-            # Record principal due in the tranche properties dictionary
-            tranche.properties[tranche.period]['Principal due'] = principalDue
+            if principalPayment != tranche.properties[tranche.period - 1]['Notional balance']:
+                principalDue = cashAmount
+                principalPayment = cashAmount
+                # Record principal due in the tranche properties dictionary
+                tranche.properties[tranche.period]['Principal due'] = principalDue
 
             # SPECIAL CASE: If the tranche is paid off, principalDue is equal to the principal payment
             if principalPayment == tranche.properties[tranche.period - 1]['Notional balance']:
@@ -143,8 +150,7 @@ class StructuredSecurities(object):
     # Method to do waterfalls for the SS object for each period until the underlying loanPool runs out of active loans
     def doWaterfallSequential(self, sequential=True):
         '''
-
-        :param sequential: User passed-in param to specify principal payout distribution
+        :param sequential: specify principal payout distribution. True for sequential, False for prorata. Default is True
         :return: None
         '''
 
@@ -175,10 +181,9 @@ class StructuredSecurities(object):
     def simulateWaterfallSequential(self, nSims, sequential=True):
         '''
         Method to run the waterfall multiple times
-        structuredSecurity: the StructuredSecurities object, usually an ABS
-        loanPool: the LoanPool object of the ABS
-        nSims: number of simulations to run
-        :return: a dictionary of each tranche's pairs-tuple (weighted RIY, weighted AL)
+        :param nSims: number of simulations to run
+        :param sequential=True: specify principal payout distribution for the tranches
+        :return: a dictionary with key = tranche, value = pairs-tuple (weighted RIY, weighted AL)
         '''
 
         # First we reset the ABS in case another waterfall has already been run on it
@@ -188,9 +193,9 @@ class StructuredSecurities(object):
         dictTuple = {}
 
         for tranche in self:
-            metricsDict[f'{tranche.subordination}'] = {}
-            metricsDict[f'{tranche.subordination}'][f'Total RIY'] = 0
-            metricsDict[f'{tranche.subordination}'][f'Total AL'] = 0
+            metricsDict[tranche] = {}
+            metricsDict[tranche][f'Total RIY'] = 0
+            metricsDict[tranche][f'Total AL'] = 0
 
 
         # doWaterfallSequential nSims times
@@ -199,13 +204,13 @@ class StructuredSecurities(object):
 
             # After the iteration is done, add it onto the metrics tally in the dictionary
             for tranche in self:
-                
-                # Add the iteration RIY to the metrics tally 
-                metricsDict[f'{tranche.subordination}'][f'Total RIY'] += tranche.RIY()[0]
+
+                # Add the iteration RIY to the metrics tally
+                metricsDict[tranche][f'Total RIY'] += tranche.RIY()[0]
 
                 # Only add the average life if the tranche is paid down
                 if tranche.AL() is not None:
-                    metricsDict[f'{tranche.subordination}'][f'Total AL'] += tranche.AL()
+                    metricsDict[tranche][f'Total AL'] += tranche.AL()
 
             # Reset the ABS and the tranches to doWaterfallSequential all over again
             self.reset()
@@ -213,8 +218,8 @@ class StructuredSecurities(object):
             # In the last iteration, calculate the weighted metrics for each tranche and store it in the metrics dictionary
             if nSim == nSims - 1:
                 for tranche in self:
-                    metricsDict[f'{tranche.subordination}'][f'Weighted RIY'] = metricsDict[f'{tranche.subordination}'][f'Total RIY'] / nSims
-                    metricsDict[f'{tranche.subordination}'][f'Weighted AL'] = metricsDict[f'{tranche.subordination}'][f'Total AL'] / nSims
+                    metricsDict[tranche][f'Weighted RIY'] = metricsDict[tranche][f'Total RIY'] / nSims
+                    metricsDict[tranche][f'Weighted AL'] = metricsDict[tranche][f'Total AL'] / nSims
 
 
         for tranche, metrics in metricsDict.items():
@@ -222,3 +227,105 @@ class StructuredSecurities(object):
             dictTuple[tranche] = (metrics['Weighted RIY'], metrics['Weighted AL'])
 
         return dictTuple
+
+    def runMonteSequential(self, nSims, tolerance, sequential=True):
+        '''
+        Method to do Monte Carlo simulation nSims times on the ABS
+        :param nSims: the number of simulations we want to run
+        :param tolerance: mechanism to specify when the Monte Carlo simulation is done: if the rates difference between each simulation is lower than tolerance, break
+        :param sequential=True: specify principal payout distribution. Default is sequential
+        :return: a dictionary with key = tranche, value = 3-items-tuples (weighted RIY, weighted AL, fair rate)
+        '''
+
+        '''
+        According to advanced numerical methods:
+        𝑛𝑒𝑤𝑇𝑟𝑎𝑛𝑐ℎ𝑒𝑅𝑎𝑡𝑒 = 𝑜𝑙𝑑𝑇𝑟𝑎𝑛𝑐ℎ𝑒𝑅𝑎𝑡𝑒 + 𝑐𝑜𝑒𝑓𝑓 ∗ (𝑦𝑖𝑒𝑙𝑑 − 𝑜𝑙𝑑𝑇𝑟𝑎𝑛𝑐ℎ𝑒𝑅𝑎𝑡𝑒)
+        𝒘𝒉𝒆𝒓𝒆 𝑐𝑜𝑒𝑓𝑓 𝑖𝑠 1.2 𝑓𝑜𝑟 𝑇𝑟𝑎𝑛𝑐ℎ𝑒 𝐴 𝑎𝑛𝑑 0.8 𝑓𝑜𝑟 𝑇𝑟𝑎𝑛𝑐ℎ𝑒 𝐵
+        '''
+        yieldsList = []
+        coefficientsList = [1.2, 0.8]
+
+        # Initialize ratesList with the original, arbitrary rate, which we'll later REFINE in the infinite loop
+        ratesList = [self.trancheList[0].rate, self.trancheList[1].rate]
+
+        while True:
+
+            # Run the Waterfall nSims times and save the tranches' AVERAGE metrics to a dictionary
+            metricsDict = self.simulateWaterfallSequential(nSims, sequential)
+
+            # Pass in those metrics into our calculateYield() function and append the yields to the yieldsList
+            for tranche, metricsTuple in metricsDict.items():
+                print(f'{tranche}: RIY: {metricsTuple[0]}, AL: {metricsTuple[1]}')
+                yieldsList.append(self.calculateYield(metricsTuple[0], metricsTuple[1]))
+
+            # Tweak the tranche rate to reflect the new yield
+            # Relaxation to speed up convergence
+            # Hardcoded 2 instead of len(self.trancheList) because our model only works with 2 tranches
+            for index in range(2):
+                '''
+                Formula
+                𝑛𝑒𝑤𝑇𝑟𝑎𝑛𝑐ℎ𝑒𝑅𝑎𝑡𝑒 = 𝑜𝑙𝑑𝑇𝑟𝑎𝑛𝑐ℎ𝑒𝑅𝑎𝑡𝑒 + 𝑐𝑜𝑒𝑓𝑓 ∗ (𝑦𝑖𝑒𝑙𝑑 − 𝑜𝑙𝑑𝑇𝑟𝑎𝑛𝑐ℎ𝑒𝑅𝑎𝑡𝑒)
+                                𝒘𝒉𝒆𝒓𝒆 𝑐𝑜𝑒𝑓𝑓 𝑖𝑠 1.2 𝑓𝑜𝑟 𝑇𝑟𝑎𝑛𝑐ℎ𝑒 𝐴 𝑎𝑛𝑑 0.8 𝑓𝑜𝑟 𝑇𝑟𝑎𝑛𝑐ℎ𝑒 𝐵
+                '''
+                oldTrancheRate = ratesList[index]
+                newTrancheRate = oldTrancheRate + coefficientsList[index] * (yieldsList[index] - oldTrancheRate)
+                ratesList.append(newTrancheRate)
+
+                # # Debug
+                # print(f'{index} old rate: {oldTrancheRate}, new rate: {newTrancheRate}, yieldsList: {yieldsList}')
+
+            'ratesList now holds 4 elements: oldTrancheRateA, oldTrancheRateB, newTrancheRateA, newTrancheRateB'
+            # Calculate difference between the new tranche rate and the old tranche rate
+            '''
+            𝑑𝑖𝑓𝑓 =(𝑛𝐴 ∗ |𝑙𝑎𝑠𝑡𝐴𝑅𝑎𝑡𝑒 − 𝑛𝑒𝑤𝐴𝑅𝑎𝑡𝑒 / 𝑙𝑎𝑠𝑡𝐴𝑅𝑎𝑡𝑒 | + 𝑛𝐵 ∗ |𝑙𝑎𝑠𝑡𝐵𝑅𝑎𝑡𝑒 − 𝑛𝑒𝑤𝐵𝑅𝑎𝑡𝑒 / 𝑙𝑎𝑠𝑡𝐵𝑅𝑎𝑡𝑒 |) / 𝑁
+                                            𝒘𝒉𝒆𝒓𝒆:
+                                            𝑛𝐴 𝑖𝑠 𝑇𝑟𝑎𝑛𝑐ℎ𝑒 𝐴 𝑛𝑜𝑡𝑖𝑜𝑛𝑎𝑙,
+                                            𝑛𝐵 𝑖𝑠 𝑇𝑟𝑎𝑛𝑐ℎ𝑒 𝐵 𝑛𝑜𝑡𝑖𝑜𝑛𝑎𝑙,
+                                            𝑁 𝑖𝑠 𝑡ℎ𝑒 𝑡𝑜𝑡𝑎𝑙 𝑛𝑜𝑡𝑖𝑜𝑛𝑎l
+            '''
+            # diffA = self.trancheList[0].notional * abs((ratesList[0] - ratesList[2])/ratesList[0]) / self.trancheList[0].notional
+            # diffB = self.trancheList[1].notional * abs((ratesList[1] - ratesList[3])/ratesList[1]) / self.trancheList[1].notional
+
+            diff = (self.trancheList[0].notional * abs((ratesList[0] - ratesList[2])/ratesList[0])
+                    + self.trancheList[1].notional * abs((ratesList[1] - ratesList[3])/ratesList[1])) / self.totalNotional
+
+            # Pass the refined rates into the Tranches and try the simulation again
+            self.trancheList[0].rate = ratesList[2]
+            self.trancheList[1].rate = ratesList[3]
+
+            # # Debug
+            # print(f'ratesList: {ratesList}, yieldsList: {yieldsList}')
+
+            # If difference is less than the tolerance we set out, our Monte Carlo is finished
+            if diff < tolerance:
+                dictTuple = {}
+                for tranche, metricsTuple in metricsDict.items():
+                    dictTuple[tranche] = (metricsTuple[0], metricsTuple[1], tranche.rate)
+                return dictTuple
+
+            # If it's not finished, remove the first 2 rates in the ratesList, and leave only the refined rates
+            # print(f'Rate A: {self.trancheList[0].rate}, Rate B: {self.trancheList[1].rate}, diffA: {diffA}, diffB: {diffB}')
+            del ratesList[0]
+            del ratesList[0]
+            # Empty the yieldsList as well
+            yieldsList = []
+
+
+
+    def calculateYield(self, RIY, AL):
+        '''
+        Method to find the yield of the ABS based on the passed in RIY and AL, based on a pre-existing yield curve model
+        :param RIY: the weighted RIY of the tranche after all the simulations have run
+        :param AL: the weighted AL of the tranche after all the simulations have run
+        :return: the yield for the tranche
+        '''
+
+        '''
+        Formula:
+        𝑦𝑖𝑒𝑙𝑑 =
+                    (7 / (1 + .08 ∗ 𝑒 ** −.19(𝑎/12)) + .019 * sqrt((𝑎/12)(𝑑 ∗ 100))) / 100
+                    𝒘𝒉𝒆𝒓𝒆 𝑎 𝑖𝑠 𝑡ℎ𝑒 𝑊𝐴𝐿 𝑎𝑛𝑑 𝑑 𝑖𝑠 𝑡ℎ𝑒 𝐷𝐼𝑅𝑅.
+        '''
+
+        numerator = 7 / (1 + .08 * (math.e ** (-0.19 * AL / 12))) + 0.19 * math.sqrt(AL * RIY * 100 / 12)
+        return numerator / 100
